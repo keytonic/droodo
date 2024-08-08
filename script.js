99592e39f6bc2cfeaac277e7a15ed8e6493e83ea
Slick McFavorite <20858630+keytonic@users.noreply.github.com>
2024-08-08 12:53:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ window.onload = loadFunc;
 
 function loadFunc(event)
 {
-    document.title = document.getElementById("text").innerText = "Boilerplate";
-    document.getElementById("aButton").onclick = clickFunc;
+
 }
 
-function clickFunc(event)
-{
-    if(event.target.id == "aButton")
-    {
-	alert("Clicked");
-        console.log("Clicked");
-    }
-}
